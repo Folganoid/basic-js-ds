618b1fa7fb1198719c7c27a32df70e11d78b220c
Andrii Holubkov <folganoid@yahoo.com>
2022-10-09 16:57:37 +0300

    feat: binary tree search

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,39 +8,141 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.pivot = null;
+  }
+
+  createNode(val) {
+    return {
+      data: val,
+      left: null,
+      right: null
+    }
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return (this.pivot === null) ? null : this.pivot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+
+    let addRec = (node, val) => {
+      if (!node) {
+        return this.createNode(val)
+      }
+
+      if (node.data === val) {
+        return node;
+      }
+
+      if (val < node.data) {
+        node.left = addRec(node.left, val);
+      }
+
+      if (val > node.data) {
+        node.right = addRec(node.right, val);
+      }
+
+      return node;
+
+    }
+    this.pivot = addRec(this.pivot, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let find = (node, val) => {
+      if (node === null) return false;
+      if (node.data === val) return true;
+      
+      if (val < node.data) {
+        return find(node.left, val);
+      }
+      
+      if (val > node.data) {
+        return find(node.right, val); 
+      }
+    }
+
+    return find(this.pivot, data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    
+    let findVal = (node, val) => {
+
+      if (node.data === val) return node;
+
+      if (val > node.data) {
+        if (node.right === null) return null;
+        return findVal(node.right, val);
+      } else {
+        if (node.left === null) return null;
+        return findVal(node.left, val);
+      }
+    }
+
+    return findVal(this.pivot, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+  
+    let removeVal = (node, val) => {
+
+      if (node === null) return null;
+    
+      if (val < node.data) {
+        node.left = removeVal(node.left, val);
+        return node;
+      } else if (val > node.data) {
+        node.right = removeVal(node.right,val);
+        return node;
+      } else {
+        if (node.left === null && node.right === null) {
+          return null;
+        }
+
+        if (node.left === null) {
+          return node.right;
+        }
+        if (node.right === null) {
+          return node.left;
+        }
+
+        let minR = node.right;
+        for (;;) {
+          if (minR.left === null) break;
+          minR = minR.left;
+        }
+        node.data = minR.data;
+        node.right = removeVal(node.right, minR.data);
+
+        return node;
+      }
+
+    }
+
+    this.pivot = removeVal(this.pivot, data);
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.pivot === null) return false;
+    let cur = this.pivot;
+    for (;;) {
+      if (cur.left === null) break;
+      cur = cur.left;
+    }
+    return cur.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.pivot === null) return false;
+    let cur = this.pivot;
+    for (;;) {
+      if (cur.right === null) break;
+      cur = cur.right;
+    }
+    return cur.data;
   }
 }
 
